8a0e94f4ec3d92a2381faaeacce54adbd446d1c9
Prakash <pkpndy@gmail.com>
2022-08-24 10:10:36 +0530

    added weather api project

---
+++ api project/script.js
@@ -0,0 +1,46 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '25d61676d6mshbdffbc26f63d92ap1bf30bjsndc17aeaf26c7',
+		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+	}
+};
+
+
+
+let getWeatherData =(city) => {
+  const URL = "https://open-weather13.p.rapidapi.com/city/";
+
+  const finalURL = `${URL}${city}`;
+  
+
+  const weatherPromise=fetch(`${finalURL}`, options);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+const showWeatherData = (weatherData) => {
+
+  document.getElementById('city-name').innerText=weatherData.name;
+  document.getElementById('weather-type').innerText=weatherData.weather[0].main;
+  document.getElementById('temp').innerText=weatherData.main.temp;
+  document.getElementById('min-temp').innerText=weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText= weatherData.main.temp_max;
+  
+}
+
+
+const searchCity =() => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city).then((res)=>{
+    showWeatherData(res);
+  }).catch((err) => {
+    console.log(err);
+    console.log("problem occured");
+  })
+  
+}
+
+
